5df6eb31cb5afee689b87c7397bf20e6d94bc244
luizrmm <luizrmm97@gmail.com>
2018-08-02 12:14:22 -0300

    config heroku

--- WoundWeb/settings.py
+++ WoundWeb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
