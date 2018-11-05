c8650032b590605e0636efff4dd4fb3604ea3768
luizrmm <luizrmm97@gmail.com>
2018-11-05 19:21:33 -0200

    djando exetensions

--- WoundWeb/settings.py
+++ WoundWeb/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrapform',
-    'core.apps.CoreConfig'
+    'core.apps.CoreConfig',
+    'django_extensions',
 ]
 
 
